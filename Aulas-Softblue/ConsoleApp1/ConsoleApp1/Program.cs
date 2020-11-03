ade2ade756d501e9385600969520aba9350a4ef0
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-11-03 18:43:07 -0300

    console

---
+++ Aulas-Softblue/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Bem vindo ao C#!")
+                
+        }
+    }
+}
