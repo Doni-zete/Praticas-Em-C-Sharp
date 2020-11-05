717e30a29e3e74e7c3835019fede7010140c460b
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-11-05 12:51:03 -0300

    console1

--- Aulas-Softblue/ConsoleApp1/ConsoleApp1/Program.cs
+++ Aulas-Softblue/ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,15 +4,22 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             double d = Math.Pow(10, 2);
 
-            if (d > 0)
+            if (d>0)
             {
 
             }
-                
+
+            for (int x = 0; x < 10; x++)
+            {
+
+            }
+
+            Console.WriteLine("tudo aqui");
+
         }
     }
 }
