d636622c33272793150e5810e064a5e0fbab3780
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-11-05 12:52:04 -0300

    Create Somar.cs

---
+++ Aulas-Softblue/ConsoleApp1/Somar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+ 	
+namespace Visual
+{
+
+	class program
+	{
+		static void Main()
+
+		{
+			int x1 = Somar(5, 7);
+			int x2 = Somar(5, 7);
+			int x3 = Somar(5, 7);
+			int x4 = Somar(5, 7);
+			int x5 = Somar(5, 7);
+		}
+		static int Somar(int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
