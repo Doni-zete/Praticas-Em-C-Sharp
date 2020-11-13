a2a25a3b4395c00debe74835479447fc7ad24ed5
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2020-11-13 15:47:58 -0300

    tes

---
+++ Aulas-Softblue/UnitTestPratica/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPratica
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestarIdadeMaior18()
+        {
+            int idade = 18;
+            bool resultado -helper.
+            Assert.IsTrue(resultado);
+        }
+    }
+    dad
+    [TestMethod]
+    public void TestarNumerioRiso()
+    {
+    }
+}
+}
